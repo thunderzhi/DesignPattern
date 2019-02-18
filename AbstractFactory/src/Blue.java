31f8f912dece19a53be7eebfab6a0e7474023e5b
williamcui <williamcui8766@hotmail.com>
2019-02-18 23:39:59 +0800

    pattern

---
+++ AbstractFactory/src/Blue.java
@@ -0,0 +1,10 @@
+
+public class Blue implements Color {
+
+	@Override
+	public void fill() {
+		// TODO Auto-generated method stub
+		System.out.println("Inside Green::fill() method.");
+	}
+
+}
---
+++ Observer/src/BinaryObserver.java
@@ -0,0 +1,14 @@
+
+public class BinaryObserver extends Observer {
+	public BinaryObserver(Subject subject){
+	      this.subject = subject;
+	      this.subject.attach(this);
+	   }
+	@Override
+	public void update() {
+		// TODO Auto-generated method stub
+		System.out.println( "Binary String: " 
+			      + Integer.toBinaryString( subject.getState() ) ); 
+	}
+
+}
