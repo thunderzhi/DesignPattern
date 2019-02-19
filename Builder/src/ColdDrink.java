ae2a365b99864bce8e0f2354abaf5c2824c17c51
thunderzhi <465785021@qq.com>
2019-02-19 23:53:37 +0800

    Builder

---
+++ Builder/src/ColdDrink.java
@@ -0,0 +1,13 @@
+
+public abstract class ColdDrink implements Item {
+ 
+	@Override
+	public Packing packing() {
+		// TODO Auto-generated method stub
+		return new Bottle();
+	}
+
+	@Override
+	public abstract float price() ;
+
+}
