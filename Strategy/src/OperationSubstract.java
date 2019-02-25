997f121b69c39d19f30084232e4ba14b0331521f
thunderzhi <465785021@qq.com>
2019-02-25 10:51:18 +0800

    Strategy

---
+++ Strategy/src/OperationSubstract.java
@@ -0,0 +1,10 @@
+
+public class OperationSubstract implements Strategy {
+
+	@Override
+	public int doOperation(int num1, int num2) {
+		// TODO Auto-generated method stub
+		return num1 - num2;
+	}
+
+}
