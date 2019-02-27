632567467902f657955099828504a522610de16f
thunderzhi <465785021@qq.com>
2019-02-27 23:23:30 +0800

    Decorator

---
+++ Decorator/src/ShapeDecorator.java
@@ -0,0 +1,12 @@
+
+public class ShapeDecorator implements Shape{
+	 protected Shape decoratedShape;
+	 
+	   public ShapeDecorator(Shape decoratedShape){
+	      this.decoratedShape = decoratedShape;
+	   }
+	 
+	   public void draw(){
+	      decoratedShape.draw();
+	   }  
+}
