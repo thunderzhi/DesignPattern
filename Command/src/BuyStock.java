b17dcb01ab96cabadeaa5bc228e7543391073806
thunderzhi <465785021@qq.com>
2019-03-05 00:18:14 +0800

    command   memo   vvisitor

---
+++ Command/src/BuyStock.java
@@ -0,0 +1,12 @@
+
+public class BuyStock implements Order {
+	 private Stock abcStock;
+	 
+	   public BuyStock(Stock abcStock){
+	      this.abcStock = abcStock;
+	   }
+	 
+	   public void execute() {
+	      abcStock.buy();
+	   }
+}
