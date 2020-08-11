319cfb60beab1f1f96729969808e06a087bf65bd
thunderzhi <465785021@qq.com>
2020-08-11 14:46:34 +0800

    add new

---
+++ Bridge/src/WhiteCircle.java
@@ -0,0 +1,11 @@
+
+public class WhiteCircle implements DrawAPI {
+
+	@Override
+	public void drawCircle(int radius, int x, int y) {
+		// TODO Auto-generated method stub
+		 System.out.println("Drawing Circle[ color: white, radius: "
+		         + radius +", x: " +x+", "+ y +"]");
+	}
+
+}
